579f52aca6e26f69b057a03d3c453ce7e96a8efd
Antoine Lamade <antoinelamade@outlook.fr>
2021-05-20 15:29:50 +0200

    michel

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -5,4 +5,5 @@ class Phone < ApplicationRecord
   validates :state, presence: true, inclusion: { in: ['Correct', 'Bon etat', 'Tres bon etat'] }
   validates :brand, presence: true, inclusion: { in: ['Apple', 'Samsung', 'Huawei', 'Xiaomi'] }
   validates :user, :start_dispo, :end_dispo, :model, :description, presence: true
+  validates :price, presence: true
 end
