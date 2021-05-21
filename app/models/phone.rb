f4bf3e13c510a4838586a2643273e3ecbf7546db
nicolas sturiano <nicolas.sturiano-leman@essca.eu>
2021-05-21 15:32:33 +0200

    front

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -4,7 +4,7 @@ class Phone < ApplicationRecord
   has_one_attached :photo
   validates :state, presence: true, inclusion: { in: ['Correct', 'Bon etat', 'Tres bon etat'] }
   validates :brand, presence: true, inclusion: { in: ['Apple', 'Samsung', 'Huawei', 'Xiaomi'] }
-  validates :model, presence: true, inclusion: { in: ['Iphone X', 'Iphone 11', 'Iphone 12', 'Galaxy', 'Note', 'Star', 'P10'] }
+  validates :model, presence: true, inclusion: { in: ['Iphone X', 'Iphone 11', 'Iphone 12', 'Galaxy', 'Note', 'Star', 'P10', 'Iphone XR', 'Iphone 8', 'Iphone SE', 'Iphone 12 pro', 'Iphone XS max', 'P20', 'P30'] }
   validates :user, :start_dispo, :end_dispo, :model, :description, presence: true
   validates :price, presence: true
   validates :address, presence: true
